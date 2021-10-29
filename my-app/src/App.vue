da4a5bb076da250044a8711352caedfc69e85d09
duanxinhuan <duanxinhuan@163.com>
2021-10-29 16:19:20 +1100

    create initial table, pass event and props

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <main-screen/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainScreen from './components/MainScreen';
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    MainScreen
+   
+  },
+
+  data: () => ({
+   
+  }
+  ),
+};
+</script>
---
+++ my-app/src/components/table/Tablecomponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-col cols="12">
+        <my-table :orders="items" @passOrderEvent="orderEvent"></my-table>
+      </v-col>
+</template>
+
+
+<script>
+import MyTable from './SimpleTable.vue'
+export default {
+     components:{
+      MyTable,
+    },
+
+    methods:{
+        orderEvent(orderStatus){
+            console.log("trigger from table component!")
+            console.log(orderStatus)
+        }
+    },
+
+    data(){
+        return{
+            items:[
+                {
+                    orderName:"#c91018",
+                    company:"happy PLT",
+                    customerName: "Jhone Lenno",
+                    orderDate: "77",
+                    deliveredAmount:33.5,
+                    totalAmount:22.9,
+                }
+            ],
+        }
+    }
+    
+}
+</script>
